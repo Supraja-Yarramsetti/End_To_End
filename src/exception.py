15e9c581934e61a38a01fe53240559e64afabed8
supraja <sspallavi99@gmial.com>
2023-03-16 15:08:20 +0530

    eda and pblm statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_message_detail.exc_info()
@@ -17,4 +17,12 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+
+if __name__ == '__main__':
+    try:
+        a =1/10
+    except Exception as e:
+        logging.info("divide by Zero")
+        raise CustomException(e,sys)
+
 
